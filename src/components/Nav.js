18843a5f33cae43216bd729b5fc1056c4c24bf41
William Truscott <williamtruscott@Williams-MacBook-Pro.local>
2021-05-04 11:23:25 -0400

    proof that i am able to change the class on my nav bar tested by changing the color at various sizes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,17 +4,49 @@ import Media from "react-media"
 const Nav = (props) => {
   return (
     <div>
-      <div className="nav">
-        <Link to="/">
-          <div>Home</div>
-        </Link>
-        <Link to="/passport">
-          <div>My Passport</div>
-        </Link>
-        <Link to="/futuredestinations">
-          <div>Future Destinations</div>
-        </Link>
-      </div>
+      <Media queries={{
+        small: "(max-width: 599px)",
+        medium: "(min-width: 600px) and (max-width: 1199px)",
+        large: "(min-width: 1200px)"
+      }}>
+        {matches => (
+          <>
+            {matches.small && <div className="navS">
+              <Link to="/">
+                <div>Home</div>
+              </Link>
+              <Link to="/passport">
+                <div>My Passport</div>
+              </Link>
+              <Link to="/futuredestinations">
+                <div>Future Destinations</div>
+              </Link>
+            </div>}
+            {matches.medium && <div className="navM">
+              <Link to="/">
+                <div>Home</div>
+              </Link>
+              <Link to="/passport">
+                <div>My Passport</div>
+              </Link>
+              <Link to="/futuredestinations">
+                <div>Future Destinations</div>
+              </Link>
+            </div>}
+            {matches.large && <div className="navL">
+              <Link to="/">
+                <div>Home</div>
+              </Link>
+              <Link to="/passport">
+                <div>My Passport</div>
+              </Link>
+              <Link to="/futuredestinations">
+                <div>Future Destinations</div>
+              </Link>
+            </div>}
+          </>
+        )}
+          </Media>
     </div>
   );
 };
